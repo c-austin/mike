a45fbb04dc51e38aad51c4278a6381ac715e7d95
Cory Austin <cory.austin@ryerson.ca>
2025-05-03 09:15:42 -0700

    progress

---
+++ public/index.js
@@ -0,0 +1,54 @@
+const HOME_SECTIONS = [
+    'home'
+];
+
+const SITE_SECTIONS = [
+    'tour',
+    'bio',
+    'contact'
+];
+
+function handleLocation() {
+    console.log("location");
+    var section = window.location.href.split('#')[1];
+    console.log(section);
+    if (SITE_SECTIONS.includes(section)) {
+        console.log("site");
+        show(SITE_SECTIONS);
+        hide(HOME_SECTIONS);
+    } else {
+        console.log("home");
+        show(HOME_SECTIONS);
+        hide(SITE_SECTIONS);
+    }
+}
+
+function show(ids) {
+    ids.forEach(id => {
+        document.getElementById(id).removeAttribute('hidden');
+    });
+}
+
+function hide(ids) {
+    ids.forEach(id => {
+        document.getElementById(id).setAttribute('hidden', true);
+    });
+}
+
+window.addEventListener('load', handleLocation);
+window.addEventListener('popstate', handleLocation);
+
+const navToggle = document.querySelector('[aria-controls="primary-nav"]');
+const primaryNav = document.querySelector('nav');
+
+
+const links = document.querySelectorAll('a');
+links.forEach(link => link.addEventListener('click', toggleAriaExpanded))
+
+navToggle.addEventListener('click', toggleAriaExpanded);
+
+function toggleAriaExpanded() {
+    console.log("click");
+    const isNavOpen = navToggle.getAttribute('aria-expanded') === 'true';
+    navToggle.setAttribute('aria-expanded', isNavOpen ? 'false' : 'true');
+}
